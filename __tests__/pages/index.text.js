977d88af2fc01d0d76b00ea43dbae37e732154c7
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2022-04-19 23:12:12 -0600

    finished configuring jest and testing library

---
+++ __tests__/pages/index.text.js
@@ -0,0 +1,19 @@
+// Modules
+import '@testing-library/jest-dom'
+import React from 'react'
+import {
+  render,
+  screen,
+} from '@testing-library/react'
+
+// Components
+import HomePage from 'pages/index'
+
+describe('Home Page', () => {
+  it('should have h1', () => {
+    render(<HomePage />)
+
+    const heading = screen.getByText('Website Coming Soon')
+    expect(heading).toBeInTheDocument()
+  })
+})
