01dee9ea5de54c0adf204e9e5ab216e5d1b55978
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2022-04-22 09:55:46 -0600

    finished hooking up theme to root styles

--- __tests__/pages/index.text.js
+++ __tests__/pages/index.text.js
@@ -9,9 +9,12 @@ import {
 // Components
 import HomePage from 'pages/index'
 
+// Context
+import GlobalContext from 'context/GlobalContext'
+
 describe('home Page', () => {
   it('should have h1', () => {
-    render(<HomePage />)
+    render(<HomePage />, { wrapper: GlobalContext })
 
     const heading = screen.getByText('Website Coming Soon')
     expect(heading).toBeInTheDocument()
