41ca9d731603ec913250bc3fc12c687477fa14ab
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2022-05-03 23:10:34 -0600

    removed ThemeWrapper

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // Components
+import Button from 'components/Button'
 import Heading from 'components/Heading'
 import MetaData from 'components/MetaData'
+import SquareImage from 'components/SquareImage'
 
 const Home = () => (
   <>
@@ -17,6 +19,13 @@ const Home = () => (
       }}
     >
       <Heading isCenter>Website Coming Soon</Heading>
+      <Button>Test Button</Button>
+      <SquareImage
+        height={400}
+        image='https://spaceholder.cc/800x400'
+        imageAlt='test alt text'
+        width={800}
+      />
     </div>
   </>
 )
