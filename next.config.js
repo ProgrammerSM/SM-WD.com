6857c295215a738223f326a30a11aad71689d4a1
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2022-09-13 22:27:06 -0600

    updated packages, updated linting config, added eslint-formatter-summary and react-proptypes-generate

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  meta: {},
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   images: {
     domains: ['spaceholder.cc'],
-  },
-  settings: {
-    jest: {
-      version: require('jest/package.json').version,
-    },
   },
 }
 
