f2bf47d2facc6f0a367b53b045b3f7190ee83b8a
PSM-Desktop <purelyfreshwateraquariums@gmail.com>
2022-12-04 19:20:39 -0700

    testing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 const exclude = ['/404', '/contact/thank-you']
 const siteUrl = process.env.SITE_URL || 'http://localhost:3000'
-console.log(process.env.NODE_ENV)
+console.log(process.env.VERCEL_ENV)
 const policies = process.env.NODE_ENV === 'production'
   ? [
       {
